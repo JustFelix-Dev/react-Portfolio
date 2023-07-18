d4396360626252de3eef0a4b6a9590d8f4cd05f8
Felix Owolabi <owolabifelix78@gmail.com>
2023-07-18 15:01:51 +0100

    react portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Main from "./pages/Main";
+import IndexPage from "./pages/IndexPage";
 
 const router = createBrowserRouter(
                createRoutesFromElements(
                 <Route path="/" element={<Main/>}>
-                   
+                  <Route index element={<IndexPage/>}/>
                 </Route>
                )
 )
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,18 +1,22 @@
-import {Link,NavLink} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
+import { Link } from 'react-scroll';
 const Nav = () => {
     return ( 
            <>
            <nav>
-            <Link to='/' className='home__link'>
+            <NavLink to='/' className='home__link'>
             <img src="NavIcon.png" alt="nav-icon" height={40} width={40}/>
-            <span className="logo__title">JustFelixDev</span>
-            </Link>
+            <span className="logo__title">JustFelix <svg width={20} height={20} fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5"></path>
+            </svg></span>
+            </NavLink>
             <div className="nav__items">
                 <ul>
                  <li><NavLink to='/'>Home</NavLink></li> 
                  <li><NavLink to='/about'>About</NavLink></li> 
                  <li><NavLink to='/resume'>Resume</NavLink></li> 
-                 <li><NavLink className='contact' to='/contact'>Contact Me</NavLink></li> 
+                 <li><Link className='contact' activeClass="active" to="contact" spy={true} smooth={true} offset={100} duration={2000}>Contact Me</Link>
+                 </li>
                 </ul>
             </div>
            </nav>
