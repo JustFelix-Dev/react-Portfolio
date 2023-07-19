95a8b587d83f202d2cf1f0b9b1b9f7521da77979
Felix Owolabi <owolabifelix78@gmail.com>
2023-07-19 11:13:33 +0100

    added a tech stack section

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -73,16 +73,65 @@ c-0.18213-0.0371-0.32324,0.0781-0.41797,0.21379l-2.64013,3.78031c-0.0586,0.084-0
             </div>
         </div>
              </div>
+             <div className="my__tech">
              <div className="introduction">
                 <h2>INTRODUCTION</h2>
                   <p> I help clients,firms or organizations build applications or solve their software engineering problems by deploying/combining the use of required technology stacks needed for such projects.
                   Let's build something extraordinary together and leave a lasting   impression on your users. Reach out to me and let's get started!
                   </p>
                   <div className="intro__cards">
-                    <div className="first"></div>
-                    <div className="second"></div>
-                    <div className="third"></div>
+                    <div >
+                        <img src="/frontendIcon.png" alt="frontEndIcon" width={60} height={60} />
+                        <span>Front-End Developer</span>
+                    </div>
+                    <div >
+                        <img src="/backendIcon.png" alt="backEndIcon" width={60} height={60} />
+                        <span>Back-End Developer</span>
+                    </div>
+                    <div>
+                        <img src="/technicalIcon.png" alt="technicalIcon" width={60} height={60} />
+                        <span>Technical  Writer</span>
+                    </div>
                   </div>
+             </div>
+             <div className="technology">
+                <h2>MY TECHNOLOGY STACKS</h2>
+                <p>I demonstrate a high level of proficiency using these stacks:</p>
+            <div className="tech__stack">
+                 <div className="frontEnd">
+                 <img src="/frontendIcon.png" alt="frontEndIcon" width={60} height={60} />
+                  <div className="frontEndIcons">
+                      <span><img src="/html.png" alt="" width={45} height={45} /></span>
+                      <span><img src="/css.png" alt="" width={45} height={45} /></span>
+                      <span><img src="/bootstrap5.svg" alt="" width={45} height={45} /></span>
+                      <span><img src="/Sasss.png" alt="" width={45} height={45} /></span>
+                      <span><img src="/tailwind.png" alt="" width={45} height={45} /></span>
+                      <span><img src="/framerMotion.jpg" alt="" width={45} height={45} /></span>
+                      <span><img src="/javascript.png" alt="" width={45} height={45} /></span>
+                      <span><img src="/typescript.png" alt="" width={45} height={45} /></span>
+                      <span><img src="/react.png" alt="" width={45} height={45} /></span>
+                      <span><img src="/redux.png" alt="" width={45} height={45} /></span>
+                  </div>
+                 </div>
+                 <div className="backEnd">
+                 <img src="/backendIcon.png" alt="backEndIcon" width={60} height={60} />
+                 <div className="backEndIcons">
+                 <span><img src="/nodejs.png" alt="" width={45} height={45} /></span>
+                 <span><img src="/expressjs.png" alt="" width={45} height={45} /></span>
+                 <span><img src="/mongoDB.png" alt="" width={45} height={45} /></span>
+                 </div>
+                 </div>
+                 <div className="devOps">
+                 <img src="/devOps.png" alt="devOpsIcon" width={60} height={60} />
+                 <div className="devOpsIcons">
+                 <span><img src="/git.png" alt="" width={45} height={45} /></span>
+                 <span><img src="/github.png" alt="" width={45} height={45} /></span>
+                 <span><img src="/vercel.png" alt="" width={45} height={45} /></span>
+                 <span><img src="/netlify.png" alt="" width={45} height={45} /></span>
+                 </div>
+                 </div>
+            </div>
+             </div>
              </div>
       </>
      );
