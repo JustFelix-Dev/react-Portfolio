7fb290dafe2bf6ab3a8ae41c2934158acb553cd0
Felix Owolabi <owolabifelix78@gmail.com>
2023-08-18 14:24:05 +0100

    Added an About Page

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ import {
 } from "react-router-dom";
 import Main from "./pages/Main";
 import IndexPage from "./pages/IndexPage";
+import AboutPage from "./pages/AboutPage";
 
 const router = createBrowserRouter(
                createRoutesFromElements(
                 <Route path="/" element={<Main/>}>
                 <Route index element={<IndexPage/>}/>
+                <Route path="/about" element={<AboutPage/>}/>
                 </Route>
                )
 )
